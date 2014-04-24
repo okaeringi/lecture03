86c6a18416e3cf9871bcbaef99cdbab9439a5ecb
okaeringi <eriokawaeri@gmail.com>
2014-04-24 12:34:06 +0900

    ボタンをひとつふやす

--- js/helloworld.js
+++ js/helloworld.js
@@ -5,3 +5,12 @@ var helloWorld = function(){
 
 var showHelloWorldButton = document.getElementById("showHelloWorld");
 showHelloWorldButton.addEventListener("click", helloWorld);
+
+
+var helloWorld = function(){
+	var message = document.getElementById("hellochiko");
+	message.setAttribute("class", "");
+};
+
+var showHelloWorldButton = document.getElementById("showHellochiko");
+showHelloWorldButton.addEventListener("click", helloWorld);
